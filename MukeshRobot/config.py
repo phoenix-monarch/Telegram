e9f43b86c8f4e853bcfea7393671323e2ab2ab73
ᴢєɴσꭙ <111524889+phoenix-monarch@users.noreply.github.com>
2023-08-05 11:02:20 +0530

    Update config.py

--- MukeshRobot/config.py
+++ MukeshRobot/config.py
@@ -9,7 +9,7 @@ class Config(object):
     TOKEN = ""  # This var used to be API_KEY but it is now TOKEN, adjust accordingly.
     OWNER_ID = 5288547197 # If you dont know, run the bot and do /id in your private chat with it, also an integer
     
-    SUPPORT_CHAT = "INCRICIBLE"  # Your own group for support, do not add the @
+    SUPPORT_CHAT = "weebs_support"  # Your own group for support, do not add the @
     START_IMG = ""
     EVENT_LOGS = ()  # Prints information like gbans, sudo promotes, AI enabled disable states that may help in debugging and shit
     MONGO_DB_URI= ""
